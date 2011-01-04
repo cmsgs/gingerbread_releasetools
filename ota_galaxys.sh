c36d4140c9ac0167776921c6ade95d1b8025b314
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-01-04 12:35:15 -0800

    added some black magic

---
+++ ota_galaxys.sh
@@ -0,0 +1,33 @@
+#/bin/bash 
+
+PRODUCT="galaxys"
+OUTDIR="../out/target/product/$PRODUCT"
+
+rm -rf temp/
+mkdir temp
+mkdir temp/system
+
+echo "Copying /system ..."
+cp -R $OUTDIR/system/ temp/
+
+echo "Copying tools for otapackage ..."
+cp -R tools/* temp/
+
+echo "Copying zImage ..."
+cp ../kernel/samsung/2.6.35/arch/arm/boot/zImage temp/zImage
+
+echo "Removing .git files"
+find temp/ -name '.git' -exec rm -r {} \;
+
+echo "Compressing otapackage ..."
+pushd temp
+zip -r ../$OUTDIR/cyanogenmod7-$PRODUCT-unsigned.zip ./
+popd
+
+echo "Signing otapackage ..."
+java -jar SignApk/signapk.jar SignApk/certificate.pem SignApk/key.pk8 $OUTDIR/cyanogenmod7-$PRODUCT-unsigned.zip $OUTDIR/cyanogenmod7-$PRODUCT.zip
+
+rm $OUTDIR/cyanogenmod7-$PRODUCT-unsigned.zip
+rm -rf temp/
+echo "cyanogenmod7-$PRODUCT.zip is at $OUTDIR"
+echo "Done."
