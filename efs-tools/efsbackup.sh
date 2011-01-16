09371f71fa9ee5e1c72147e6a22fec19f8005ce0
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-01-16 06:12:08 -0800

    added efs backup tool

---
+++ efs-tools/efsbackup.sh
@@ -0,0 +1,12 @@
+#!/sbin/busybox sh
+#
+# EFS BACKUP TOOL
+# by codeworkx, coolya, finghin (c) 2010, GPLv2
+#
+
+mkdir /mnt/sdcard/backup
+mkdir /mnt/sdcard/backup/efs
+
+cp -R /efs/ /mnt/sdcard/backup/efs
+
+exit
---
+++ efsbackup_galaxys.sh
@@ -0,0 +1,27 @@
+#/bin/bash 
+
+PRODUCT="galaxys"
+OUTDIR="../out/target/product/$PRODUCT"
+
+rm -rf temp/
+mkdir temp
+mkdir temp/system
+
+echo "Copying tools for otapackage ..."
+cp -R efs-tools/* temp/
+
+echo "Removing .git files"
+find temp/ -name '.git' -exec rm -r {} \;
+
+echo "Compressing otapackage ..."
+pushd temp
+zip -r ../$OUTDIR/$PRODUCT-efsbackup-unsigned.zip ./
+popd
+
+echo "Signing otapackage ..."
+java -jar SignApk/signapk.jar SignApk/certificate.pem SignApk/key.pk8 $OUTDIR/$PRODUCT-efsbackup-unsigned.zip $OUTDIR/$PRODUCT-efsbackup.zip
+
+rm $OUTDIR/$PRODUCT-efsbackup-unsigned.zip
+rm -rf temp/
+echo "$PRODUCT-efsbackup.zip is at $OUTDIR"
+echo "Done."
