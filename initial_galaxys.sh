e0129ef199330c3749b2f86d7b5812039177d9d5
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-01-16 05:54:26 -0800

    added initial updater-script

---
+++ initial_galaxys.sh
@@ -0,0 +1,36 @@
+#/bin/bash 
+
+PRODUCT="galaxys"
+OUTDIR="../out/target/product/$PRODUCT"
+
+rm -rf temp/
+mkdir temp
+mkdir temp/system
+
+echo "Copying /system ..."
+cp -R $OUTDIR/system/ temp/
+
+echo "Copying tools for otapackage ..."
+cp -R initial-tools/* temp/
+
+echo "Copying zImage ..."
+cp ../kernel/samsung/2.6.35/arch/arm/boot/zImage temp/zImage
+
+echo "Copying kernel modules ..."
+cp -R ../kernel/samsung/2.6.35/drivers/net/wireless/bcm4329/bcm4329.ko temp/system/modules/
+
+echo "Removing .git files"
+find temp/ -name '.git' -exec rm -r {} \;
+
+echo "Compressing otapackage ..."
+pushd temp
+zip -r ../$OUTDIR/cyanogenmod7-$PRODUCT-initial-unsigned.zip ./
+popd
+
+echo "Signing otapackage ..."
+java -jar SignApk/signapk.jar SignApk/certificate.pem SignApk/key.pk8 $OUTDIR/cyanogenmod7-$PRODUCT-initial-unsigned.zip $OUTDIR/cyanogenmod7-$PRODUCT-initial.zip
+
+rm $OUTDIR/cyanogenmod7-$PRODUCT-initial-unsigned.zip
+rm -rf temp/
+echo "cyanogenmod7-$PRODUCT.zip is at $OUTDIR"
+echo "Done."
